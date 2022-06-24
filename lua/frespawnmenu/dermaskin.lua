5b2e32e801bbde30286d4ed2ba347e0722d059e1
Freline <darkfated@icloud.com>
2022-06-24 05:40:53 +0300

    Code optimization

--- lua/frespawnmenu/dermaskin.lua
+++ lua/frespawnmenu/dermaskin.lua
@@ -403,7 +403,6 @@ end
 --[[---------------------------------------------------------
 	Menu
 -----------------------------------------------------------]]
-local color_gray = Color(70,70,70,200)
 local color_panel_dm = Color(230,230,230)
 
 function SKIN:PaintMenu( panel, w, h )
@@ -778,8 +777,10 @@ local function PaintNotches( x, y, w, h, num )
 	end
 end
 
+local color_slider = Color(0,0,0,100)
+
 function SKIN:PaintNumSlider( panel, w, h )
-	surface.SetDrawColor( Color( 0, 0, 0, 100 ) )
+	surface.SetDrawColor( color_slider )
 	surface.DrawRect( 8, h / 2 - 1, w - 15, 1 )
 
 	PaintNotches( 8, h / 2 - 1, w - 16, 1, panel.m_iNotches )
